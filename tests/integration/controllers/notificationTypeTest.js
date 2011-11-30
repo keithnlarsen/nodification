d3e28ee03bd6bb6dd7c3e4a21415c181b64c0570
keithnlarsen <keithnlarsen@gmail.com>
2011-11-29 21:47:44 -0700

    Adding some stuff to the Readme, tweaking some tests

--- tests/integration/controllers/notificationTypeTest.js
+++ tests/integration/controllers/notificationTypeTest.js
@@ -1,8 +1,8 @@
 describe('Nodification.Tests.Integration.Controllers.NotificationTypeController', function(){
 
   var should = require('should');
-  var createJSON = "{\"name\":\"Test\",\"registrationUrl\":\"url\",\"userName\": \"username\",\"password\": \"password\"}";
-  var updateJSON = "{\"name\":\"Test2\",\"registrationUrl\":\"url2\",\"userName\": \"username2\",\"password\": \"password2\"}";
+  var createJSON = JSON.parse("{\"name\":\"Test\",\"registrationUrl\":\"url\",\"userName\": \"username\",\"password\": \"password\"}");
+  var updateJSON = JSON.parse("{\"name\":\"Test2\",\"registrationUrl\":\"url2\",\"userName\": \"username2\",\"password\": \"password2\"}");
   var mongoose = require('mongoose');
   var newId = '';
   var notificationType;
@@ -28,8 +28,8 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
 
   describe('.insert(JSON, fn)', function(){
     it('should create a new NotificationType', function(done){
-      controller.insert(JSON.parse(createJSON), function(err, actual) {
-        var expected = JSON.parse(createJSON);
+      controller.insert(createJSON, function(err, actual) {
+        var expected = createJSON;
 
         newId = actual._id.toString();
         newId.should.be.a('string');
@@ -42,7 +42,7 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
       });
     });
     it('should fail to insert a duplicate name record', function(done){
-      controller.insert(JSON.parse(createJSON), function(err, actual) {
+      controller.insert(createJSON, function(err, actual) {
         should.exist(err);
         err.message.should.match(/duplicate key error/);
         done();
@@ -53,7 +53,7 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
   describe('.get(Id, fn)', function(){
     it('should return a single NotificationType when given an existing Id', function(done){
       controller.get(newId, function(err, actual) {
-        var expected = JSON.parse(createJSON);
+        var expected = createJSON;
 
         actual.name.should.equal(expected.name);
         actual.registrationUrl.should.equal(expected.registrationUrl);
@@ -75,7 +75,7 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
   describe('.getByName(name, fn)',function(){
     it('should return a single NotificationType when given an existing name', function(done){
       controller.getByName('Test', function(err, actual) {
-        var expected = JSON.parse(createJSON);
+        var expected = createJSON;
 
         actual.name.should.equal(expected.name);
         actual.registrationUrl.should.equal(expected.registrationUrl);
@@ -89,8 +89,8 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
 
   describe('.update(Id, JSON, fn)', function(){
     it('should update an existing NotificationType', function(done){
-      controller.update(newId, JSON.parse(updateJSON), function(err, actual) {
-        var expected = JSON.parse(updateJSON);
+      controller.update(newId, updateJSON, function(err, actual) {
+        var expected = updateJSON;
 
         actual.name.should.equal(expected.name);
         actual.registrationUrl.should.equal(expected.registrationUrl);
@@ -101,7 +101,7 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
       });
     });
     it('should return null when trying to Update a NotificationType That Doesn\'t Exist', function(done){
-      controller.update(new mongoose.Types.ObjectId('111111111111111111111111'), JSON.parse(updateJSON), function(err, actual) {
+      controller.update(new mongoose.Types.ObjectId('111111111111111111111111'), updateJSON, function(err, actual) {
         should.not.exist(actual);
         done(err);
       });
@@ -112,7 +112,7 @@ describe('Nodification.Tests.Integration.Controllers.NotificationTypeController'
     it('should return all NotificationTypes', function(done){
       controller.list(function(err, actual) {
         // TODO: figure out how to test for array
-        //actual.should.be.a("array");
+        actual.constructor.name.should.equal('Array');
         actual.length.should.be.greaterThan(0);
         done(err);
       });
